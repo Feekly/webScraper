0620f22962468b815518d255c9b1e3dc59454645
Feekly <149200418+Feekly@users.noreply.github.com>
2024-12-10 15:05:48 -0800

    add search bar to get user input results.html displays results. add slight css

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
 import csv
-
-from flask import Flask, render_template
-import _csv
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+def scrapeUrl(url):
+    import requests
+    from bs4 import BeautifulSoup
 
+    x = requests.get(url)
+    soup = BeautifulSoup(x.content, "html.parser")
 
-articles = []
-with open('data.csv', 'r', encoding='utf-8') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        articles.append(row)
+    #getting text
+    title = soup.find('h1').text
+    publicationDate = soup.find('time').text
+    author = soup.find(class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ BaseLink-eNWuiM BylineLink-gEnFiw iUEiRd euNVPR jdMSdZ BDKtv byline__name-link button").text
+    p = soup.find('p').text
+    paywall = soup.find(class_="paywall").text
+    return {
+        'title': title,
+        'date': publicationDate,
+        'author': author,
+        'p': p,
+        'paywall': paywall
+    }
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=articles)
+    return render_template('index.html')
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    url = request.form.get('url')
+    if not url:
+        return "No URL provided"
+    data = scrapeUrl(url)
+    return render_template('results.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+def scrapeUrl(url):
+    import requests
+    from bs4 import BeautifulSoup
 
-def toTextFile(theTitle, thePublicationDate, theAuthor, theP, thePaywall):
+    x = requests.get(url)
+    soup = BeautifulSoup(x.content, "html.parser")
+
+    #getting text
+    title = soup.find('h1').text
+    publicationDate = soup.find('time').text
+    author = soup.find(class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ BaseLink-eNWuiM BylineLink-gEnFiw iUEiRd euNVPR jdMSdZ BDKtv byline__name-link button").text
+    p = soup.find('p').text
+    paywall = soup.find(class_="paywall").text
+
     file = open("data.csv", "w")
     #header row for dictReader
     file.write("title,date,author,p,paywall\n")
@@ -11,17 +21,4 @@ def toTextFile(theTitle, thePublicationDate, theAuthor, theP, thePaywall):
     file.write('"' + p + '",')
     file.write('"' + paywall + '"')
     file.close()
-
-url = "https://www.wired.com/story/luigi-mangione-arrested-uhc-shooting/"
-x = requests.get(url)
-soup = BeautifulSoup(x.content, "html.parser")
-
-#getting text
-title = soup.find('h1').text
-publicationDate = soup.find('time').text
-author = soup.find(class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ BaseLink-eNWuiM BylineLink-gEnFiw iUEiRd euNVPR jdMSdZ BDKtv byline__name-link button").text
-p = soup.find('p').text
-paywall = soup.find(class_="paywall").text
-
-toTextFile(title, publicationDate, author, p, paywall)
 
